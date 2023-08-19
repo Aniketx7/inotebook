073b78294cfce5057979be3df9ac86ca480ba1bf
Aniket <aniket@verts.com>
2023-08-19 20:41:16 +0530

    Create a login Endpoint

--- backend_inotebook/routes/auth.js
+++ backend_inotebook/routes/auth.js
@@ -2,6 +2,9 @@ const express = require('express');
 const User = require('../models/User');         //import User.js from models 
 const { body, validationResult } = require('express-validator');
 var bcrypt = require('bcryptjs');          //bcryps js used to hash passwrod 
+var jwt = require('jsonwebtoken');          //jsw fascilate a secure connection between user and backend
+
+const JWT_SIGN = '@anikEt2389$$Kr'
 
 const router = express.Router()     //ham router express ke Router function ko router const ko pkda diye
 
@@ -24,8 +27,8 @@ router.post('/createUser', [
 
     // Now, user schema me request body save hoke database me jayega 
     try {
-        let user = await User.findOne({ email: req.body.email });          //Find if there is an another unique email in database
-        if (user) {                                                       //If it return true, send bad request and json error 
+        let findUser = await User.findOne({ email: req.body.email });          //Find if there is an another unique email in database
+        if (findUser) {                                                       //If it return true, send bad request and json error 
             return res.status(500).json({ error: 'Email already exists', })
         }
 
@@ -36,26 +39,88 @@ router.post('/createUser', [
         const secPass = await bcrypt.hash(req.body.password, salt)
 
         user = await User.create({       //create user(User is  already imported), create is express-validator function 
-            
+
             //Important-- you must use key name as yours in User schame 
 
             name: req.body.name,                        //get name from req.body(jo ham request post kiye hai)
             email: req.body.email,
             password: secPass
         })
-        res.json(user)
+
+
+        //A objct which takes id from mogno
+        const data = {
+            userId: {
+                id: user.id
+            }
+        }
+
+        var authtoken = jwt.sign(data, JWT_SIGN);       //jwt sign 
+
+        res.json({ authtoken })
     }
     catch (error) {
         console.log(error.message)
     }
 
     // After that your user data(which you provide using post method) post on database
+});
+
+
+///////////////////////////////////////
+
+// Creating a login endpints
+
+///////////////////////////////////////
+
+// login a user using: POST "api/auth/login" Doesn't require authentication
+
+router.post('/login', [
+    // using express validator                   (maine body ko express-validator pkg se import kiya hai )
+    body("email").isEmail(),                     //says, email ek valid email hona chahiye 
+    body("password").exists()
+
+], async (req, res) => {
+
+
+    const errors = validationResult(req)                //getting request body object
+    if (!errors.isEmpty()) {                            //if errors var (request body obj) become invalid, ...
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body
+    try {
+        let findUser = await User.findOne({ email });          //Find if there is an another unique email in database
+        if (!findUser) {                                                       //If it return true, send bad request and json error 
+            return res.status(400).json({ error: 'Login with valid credentials', })
+        }
+
+        // comparing password of user and of database password using bcrypt js 
+        const comparePassword = await bcrypt.compare(password, user.password)
+        if (!comparePassword) {
+            return res.status(400).json({ error: 'Incorrect Password', })
+
+        }
+
+        const data = {
+            userId: {
+                id: user.id
+            }
+        }
+
+        var authtoken = jwt.sign(data, JWT_SIGN);       //jwt sign 
+
+        res.json({ authtoken })
+
+
+    }
+    catch (error) {
+        console.log(error.message)
+
+    }
+
+
 })
-
-
-
-
-
 
 
 module.exports = router
